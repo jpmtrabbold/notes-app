69ce2d8206f7d8fca95bff6cbc5e61c858733651
João Pedro M. Trabbold <jpmtrabbold@gmail.com>
2021-03-09 10:33:23 +1300

    api starting point

---
+++ api/src/handler.ts
@@ -0,0 +1,30 @@
+import express, { Application } from "express";
+import morgan from "morgan";
+
+import 'source-map-support/register'
+
+import ServerlessHttp from "serverless-http";
+import { RegisterRoutes } from "./routes/routes";
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(express.static("public"));
+
+// TODO: swagger interface
+//import swaggerUi from "swagger-ui-express";
+// app.use(
+//     "/swagger",
+//     swaggerUi.serve,
+//     swaggerUi.setup(undefined, {
+//         swaggerOptions: {
+//             url: "/swagger.json",
+//         },
+//     })
+// );
+
+//app.use(Router);
+RegisterRoutes(app);
+
+export const handler = ServerlessHttp(app);
